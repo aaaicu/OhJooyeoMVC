32a68ed3b68daf537bc743f50311a9e131845d5b
kyome <aaaicu@naver.com>
2018-08-15 23:49:14 +0900

    feat : 예배 리스트 요청 controller 추가 (재업로드) 1. getWorshipList 추가

---
+++ src/main/java/kr/co/ohjooyeo/service/WorshipService.java
@@ -0,0 +1,21 @@
+package kr.co.ohjooyeo.service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import kr.co.ohjooyeo.dao.WorshipDAO;
+
+@Service
+public class WorshipService {
+	@Autowired
+	WorshipDAO worshipDAO;
+	
+	public List<Map<String, String>> getWorshipList() {
+		return worshipDAO.getWorshipList();
+	}
+
+}
