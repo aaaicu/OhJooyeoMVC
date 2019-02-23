40eb75dc082bd516d7bb1b91feaca3f5b4262387
kyome <aaaicu@naver.com>
2019-02-23 18:16:40 +0900

    Update : 순서 드래그 적용 1. Bootstrap을 적용한 순서에 대한 드래그 기능 적용 2. 다른 영역에 드랍할 경우 순서변경되지않도록 함 3. 수정 상태에서 순서 변경할 수 없도록 함

--- src/main/webapp/js/order-manage.js
+++ src/main/webapp/js/order-manage.js
@@ -1,18 +1,36 @@
 
 	let dragElem = null;
+	let parentArea = null;
+	let areaCheck = false;
+
+	document.getElementById('order-area');
 
 	function handleDragStart(e){
-	    dragElem = this;
-	    console.log("드래그 스타트 : ",e);
+		dragElem = null;
 
+	    // console.log("드래그 스타트 : ",e);
 //	    console.log("this : ", this);
-	    console.dir(this);
-	    e.dataTransfer.setData("text","옮김");
-	    e.dataTransfer.effectAllowed='move';
+	    if(this.dataset["editYn"] === "0"){
+			dragElem = this;
+			parentArea = this.parentNode;
+			e.dataTransfer.setData("text","옮김");
+			e.dataTransfer.effectAllowed='move';
+		}else {
+			alert("저장되지 않은 순서입니다.")
+		}
 	}
 
 	function handleDragEnter(e){
-	    console.log("드래그 앤터 : ",e);
+	    // console.log("드래그 앤터 : ",e.path);
+		areaCheck = false;
+
+	    for( let pathIndex in e.path){
+			if(parentArea == e.path[pathIndex]){
+				areaCheck = true;
+			}
+		}
+		console.log("대상? : ",areaCheck	);
+
 	}
 
 	function handleDragOver(e){
@@ -20,20 +38,22 @@
 	    if(e.preventDefault()){
 	        e.preventDefault();
 	    }
-	    this.classList.add('over');
+		if(areaCheck) {
+			this.classList.add('over');
+		}
 	    // e.dataTransfer.dropEffect = 'move';
 	    // console.log("드래그 오버 : ",e);
 	}
 	function handleDragLeave(e){
 	    this.classList.remove('over');
-	    console.log("드래그 리브 : ",e);
+	    // console.log("드래그 리브 : ",e);
 	}
 	function handleDrop(e){
 	    if(e.stopPropagation){
 	        e.stopPropagation()
 	    }
 
-	    if(dragElem != this) {
+	    if(dragElem !== this && dragElem !== null && areaCheck ) {
 	        this.parentNode.removeChild(dragElem);
 	        this.insertAdjacentHTML('beforebegin',dragElem.outerHTML);
 	        let dropElem = this.previousSibling;
@@ -41,17 +61,18 @@
 	    }
 	    this.classList.remove('over');
 	    console.log(e.dataTransfer.getData("text"));
-	    console.log("드래그 드랍 : ",e);
-	    console.log("this : ", this);
-	    console.log("this : ", this);
+	    // console.log("드래그 드랍 : ",e);
+	    // console.log("this : ", this);
 	}
 	function handleDragEnd(e){
-	    console.log("드래그 엔드 : ",e);
-	    console.log("this : ", this);
+	    // console.log("드래그 엔드 : ",e);
+	    // console.log("this : ", this);
 	}
 
 function addHandlers(elem){
 	console.log(elem);
+	elem.getElementsByClassName('modify-btn')[0].addEventListener('click', modifyCard);
+
     elem.addEventListener('dragstart',handleDragStart,false);
     elem.addEventListener('dragenter',handleDragEnter,false);
     elem.addEventListener('dragover',handleDragOver,false);
